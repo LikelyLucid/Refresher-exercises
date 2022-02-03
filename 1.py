c9d58e114a748cc1c82d92738e06b60c7970720f
PurelyStupidity <micoolplays@gmail.com>
2022-02-04 11:56:00 +1300

    12

--- 1.py
+++ 1.py
@@ -1,14 +1,8 @@
-"""Write a program which gets the user to input two numbers.
-If the numbers have different values, output the higher value otherwise output a message saying they are equal.
-The program must repeatedly ask for two numbers until a rogue number of 0 is entered. """
+
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter another number: "))
 
 if number1 == 0 or number2 == 0:
     print("You have entered 0, exiting program.")
-elif number1 > number2:
-    print(f"{number1} is greater than {number2}")
-elif number1 < number2:
-    print(f"{number2} is greater than {number1}")
 else:
-    print(f"{number1} and {number2} are equal")
+    print("The higher number is: ", max(number1, number2))
--- 3.py
+++ 3.py
@@ -1,3 +1,34 @@
+
 concrete_thickness = 0.5  # meters
 
-building_type = input("Enter the building type: ")
+
+def integer_checker(question):
+    """This function checks if an input is an integer"""
+    while True:
+        try:
+            number = float(input(question))
+            return number
+        except ValueError:
+
+            if number == "X": exit()
+            print("Please enter a valid integer")
+
+
+def float_checker(question):
+    """This function checks if an input is a float"""
+    while True:
+        try:
+            number = float(input(question))
+            return number
+        except ValueError:
+            print("Please enter a valid float")
+
+
+building_type = integer_checker("Enter the building type: \n1 = Resdential\n2 = Commercial\n")
+length = float_checker("Enter the length of the building in meters: ")
+width = float_checker("Enter the width of the building in meters: ")
+depth = float_checker("Enter the depth of the building in meters: ")
+volume = length * width * depth
+print(
+    "The volume of concrete required for a slab with a length of {length} and width of {width} and a depth of {depth} is {volume} cubic metres.".format(
+        length=length, width=width, depth=depth, volume=volume))
