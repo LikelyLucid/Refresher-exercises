300c8bf33971dbdc8a6e2e30d6025cb0c1909cb0
PurelyStupidity <micoolplays@gmail.com>
2022-02-04 15:09:28 +1300

    k

--- 3.py
+++ 3.py
@@ -1,16 +1,18 @@
-
 concrete_thickness = 0.5  # meters
+res_concrete_thickness = concrete_thickness / 2
 
 
 def integer_checker(question):
     """This function checks if an input is an integer"""
     while True:
         try:
-            number = float(input(question))
-            return number
+            number = input(question)
+            if number == "X" or "x":
+                exit()
+            else:
+                number = int(number)
+                return number
         except ValueError:
-
-            if number == "X": exit()
             print("Please enter a valid integer")
 
 
@@ -24,11 +26,15 @@ def float_checker(question):
             print("Please enter a valid float")
 
 
-building_type = integer_checker("Enter the building type: \n1 = Resdential\n2 = Commercial\n")
-length = float_checker("Enter the length of the building in meters: ")
-width = float_checker("Enter the width of the building in meters: ")
-depth = float_checker("Enter the depth of the building in meters: ")
-volume = length * width * depth
-print(
-    "The volume of concrete required for a slab with a length of {length} and width of {width} and a depth of {depth} is {volume} cubic metres.".format(
-        length=length, width=width, depth=depth, volume=volume))
+while True:
+    building_type = integer_checker("Enter the building type: \n1 = Resdential\n2 = Commercial\n")
+    length = float_checker("Enter the length of the building in meters: ")
+    width = float_checker("Enter the width of the building in meters: ")
+    if building_type == 1:
+        depth = res_concrete_thickness
+    else:
+        depth = concrete_thickness
+    volume = length * width * depth
+    print(
+        "The volume of concrete required for a slab with a length of {length} and width of {width} and a depth of {depth} is {volume} cubic metres.".format(
+            length=length, width=width, depth=depth, volume=volume))
---
+++ 4.py
@@ -0,0 +1,11 @@
+"""Your English teacher wants a program to keep track of their student marks and grades. Write a program which takes, as input, the student's name and an exam mark — between 0 and 100. The program should keep asking for input until "X" is entered as the student name. The program should then output the name and mark of the best student, as well as the average mark for all students.
+"""
+students = []
+while True:
+    Student = input("Enter the name of the student: ")
+    if Student == "X":
+        break
+    Grade = int(input("Enter the grade of the student: "))
+    students.append([Student, Grade])
+print("The average grade is:", sum(student[1] for student in students) / len(students))
+print("The best grade is:", max(student[1] for student in students))
