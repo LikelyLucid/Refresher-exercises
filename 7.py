26de88fc15ccc62ea18d7a4f84b8cef845543973
PurelyStupidity <micoolplays@gmail.com>
2022-02-06 12:39:50 +1300

    February 6, 2022 12:39 PM

--- 7.py
+++ 7.py
@@ -1,7 +1,5 @@
 # Get the number of seconds between lightning and thunder
 seconds = float(
     input("Enter the number of seconds between lightning and thunder: "))
-
-# Calculate the distance of the storm
 distance = seconds * 340
 print("The distance of the storm is", distance, "meters")
