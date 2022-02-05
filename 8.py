3144a2bb516f7243cdd491927b352d5f2b3d9e56
PurelyStupidity <micoolplays@gmail.com>
2022-02-06 12:45:56 +1300

    February 6, 2022 12:45 PM

--- 8.py
+++ 8.py
@@ -6,4 +6,4 @@ area = 4 * math.pi * radius ** 2
 volume = 4 / 3 * math.pi * radius ** 3
 
 print("Area =", area, "square centimetres")
-print("Volume =", volume, "cubic centimetres")
+print("Volume =", volume, "cubic centimetres")5
