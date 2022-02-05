e0079a2e58e25ce4feb171fa8e993616262de2c8
PurelyStupidity <micoolplays@gmail.com>
2022-02-05 21:19:14 +1300

    February 5, 2022 9:19 PM

--- 4.py
+++ 4.py
@@ -15,7 +15,7 @@ for student in students:
     student = student[0]
     grade = student[1]
 
-    if grade > 90:
+    if grade >= 90:
         final_grade = "A+"
     elif grade > 85:
         final_grade = "A"
