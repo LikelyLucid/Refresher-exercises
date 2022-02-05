80c2525658a55ae2f60199e565b50cfa8f949053
PurelyStupidity <micoolplays@gmail.com>
2022-02-05 21:15:53 +1300

    February 5, 2022 9:15 PM

--- 4.py
+++ 4.py
@@ -1,5 +1,6 @@
 """Your English teacher wants a program to keep track of their student marks and grades. Write a program which takes, as input, the student's name and an exam mark — between 0 and 100. The program should keep asking for input until "X" is entered as the student name. The program should then output the name and mark of the best student, as well as the average mark for all students.
 """
+
 students = []
 while True:
     Student = input("Enter the name of the student: ")
@@ -10,5 +11,5 @@ while True:
 print("The average grade is:", sum(student[1] for student in students) / len(students))
 print("The best grade is:", max(student[1] for student in students))
 
-for i in range (len(students)):
-    print("The grade of", students[i][0], "is", students[i][1])
+for student in students:
+    print("The grade of", student[0], "is", student[1])
