91d8866c67647a543827de2d12b19e89995b608d
PurelyStupidity <micoolplays@gmail.com>
2022-02-06 12:49:13 +1300

    February 6, 2022 12:49 PM

--- 4-5.py
+++ 4-5.py
@@ -1,5 +1,3 @@
-"""Your English teacher wants a program to keep track of their student marks and grades. Write a program which takes, as input, the student's name and an exam mark — between 0 and 100. The program should keep asking for input until "X" is entered as the student name. The program should then output the name and mark of the best student, as well as the average mark for all students.
-"""
 
 students = []
 while True:
