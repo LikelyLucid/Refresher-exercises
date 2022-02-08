f6e40aa6f29d08dd83469691999695e6a68694fa
PurelyStupidity <micoolplays@gmail.com>
2022-02-08 13:56:50 +1300

    February 8, 2022 1:56 PM

--- tetris.py
+++ tetris.py
@@ -1,17 +1,1 @@
-# make a playable game of tetris using
-import pygame
-import random
-from pygame.locals import *
-from sys import exit
-
-pygame.init()
-
-screen = pygame.display.set_mode((640, 480), 0, 32)
-pygame.display.set_caption("Tetris")
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
+# make a
