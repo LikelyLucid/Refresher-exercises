37bfa8f7af5c7eb80c7da225aa54607a5e363d95
PurelyStupidity <micoolplays@gmail.com>
2022-02-08 13:57:39 +1300

    February 8, 2022 1:57 PM

--- tetris.py
+++ tetris.py
@@ -14,3 +14,8 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+
+# Creating window
+screen_width = 900
+screen_height = 600
+gameWindow = pygame.display.set_mode((screen_width, screen_height))
