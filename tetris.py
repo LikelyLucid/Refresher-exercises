c61b69b5277ca86700bfc9c29004aaca078559be
PurelyStupidity <micoolplays@gmail.com>
2022-02-08 13:56:07 +1300

    February 8, 2022 1:56 PM

--- tetris.py
+++ tetris.py
@@ -8,3 +8,12 @@ pygame.init()
 
 screen = pygame.display.set_mode((640, 480), 0, 32)
 pygame.display.set_caption("Tetris")
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+piece_list = [
+    [[1, 1, 1, 1],
