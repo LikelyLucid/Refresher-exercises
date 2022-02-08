09f864d6ba0168d0fddeb16403703f32bddfd451
PurelyStupidity <micoolplays@gmail.com>
2022-02-08 13:34:15 +1300

    February 8, 2022 1:34 PM

--- 9.py
+++ 9.py
@@ -1,6 +1,3 @@
 # function that only allows numbers to be entered
 def numbers_only(string):
-    if string.isdigit():
-        return True
-    else:
-        return False
+    return bool(string.isdigit())
