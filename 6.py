fa3e79d57edf6ebb8a2abb76e03a4da5ad40f8ac
PurelyStupidity <micoolplays@gmail.com>
2022-02-08 13:29:38 +1300

    February 8, 2022 1:29 PM

--- 6.py
+++ 6.py
@@ -2,7 +2,7 @@
 biking_cal = 200
 jogging_cal = 475
 swimming_cal = 275
-# all hours
+# all in hours
 weight_lost = 454 / 3500 # mg / calories
 
 bike_hours = int(input("How many hours did you bike? "))
