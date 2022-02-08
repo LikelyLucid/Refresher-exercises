c4e624d32faaa22356622dd5d18321a66b7cc9d7
PurelyStupidity <micoolplays@gmail.com>
2022-02-08 13:31:52 +1300

    February 8, 2022 1:31 PM

--- 8.py
+++ 8.py
@@ -2,7 +2,7 @@ pi = 3.14159265359
 import math
 radius = float(input("Enter the radius of the sphere: "))
 
-area = round(4 * pi * radius ** 2, 2)
+area = round(4 * math.pi * radius ** 2, 2)
 volume = round(4 / 3 * pi * radius ** 3, 2)
 
 print("Area =", area, "square centimetres")
