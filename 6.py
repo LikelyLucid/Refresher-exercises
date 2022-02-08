5e8b569705421777b6faa4399a8c2c9aa093d815
PurelyStupidity <micoolplays@gmail.com>
2022-02-08 13:29:13 +1300

    February 8, 2022 1:29 PM

--- 6.py
+++ 6.py
@@ -9,7 +9,7 @@ bike_hours = int(input("How many hours did you bike? "))
 jogging_hours = int(input("How many hours did you jog? "))
 swimming_hours = int(input("How many hours did you swim? "))
 
-total_calories = bike_hours * biking_cal + jogging_hours * jogging_cal + swimming_hours * swimming_cal
+total_calories = bike_hours * biking_cal + jogging_hours * jogging_cal + swimming_hours * swimming_cal # add them all together
 
 print("you lost", round(total_calories * weight_lost / 1000, 3), "kg")
 
