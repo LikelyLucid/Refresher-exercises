a1aefe5ae876bc1fcc92dc042f54fea17d15a4ea
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:10:21 +1300

    February 14, 2022 2:10 PM

--- ex3_starts_with_a.py
+++ ex3_starts_with_a.py
@@ -1,4 +1,4 @@
-# Write a function statts_with_A(word) that prints true if the given word starts to "A", or false if it does not.
+# Write a function starts_with_A(word) that prints true if the given word starts to "A", or false if it does not.
 
 def starts_with_a(word):
     word.upper()
