4ddae8b86806d24287eb3ccf6c68e87a8ca1a297
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:11:08 +1300

    February 14, 2022 2:11 PM

--- ex4.py
+++ ex4.py
@@ -1,1 +1,3 @@
-#Write a function check_factor(numl, num2) that takes two numbers and prints if the second number is a factor ot the first. See the examples below:
+#Write a function check_factor(numl, num2) that takes two numbers and prints if the second number is a factor ot the first.
+def FunctionName(args):
+    
