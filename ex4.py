0e0a4fcece4276d603a1d3a200a9c0f89cdcecb8
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:11:48 +1300

    February 14, 2022 2:11 PM

--- ex4.py
+++ ex4.py
@@ -1,9 +1,6 @@
 #Write a function check_factor(numl, num2) that takes two numbers and prints if the second number is a factor ot the first.
 def check_factor(num1, num2):
-    if num1 % num2 == 0:
-        return True
-    else:
-        return False
+    return num1 % num2 == 0
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 
