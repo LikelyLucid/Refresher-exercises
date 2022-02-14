587d18dc4c5e703106f27b5dc400bac94a28397b
PurelyStupidity <micoolplays@gmail.com>
2022-02-15 08:13:13 +1300

    February 15, 2022 8:13 AM

--- ex9.py
+++ ex9.py
@@ -1,3 +1,3 @@
 """Write a function longest_word(string_list) that is given a list of words and prints the longest word in the list. If there are two words of the same length, print the first one:"""
 def longest_word(string_list):
-    ret
+    return 
