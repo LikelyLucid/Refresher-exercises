8184138541725f7c0c45a5f2cd349b1d500e9fd2
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:19:08 +1300

    February 14, 2022 2:19 PM

--- ex5.py
+++ ex5.py
@@ -8,4 +8,4 @@ def numbers_in_list(list, multiple):
         if num % multiple == 0:
             new_list.append(num)
     return new_list
-pr
+print(numbers_in_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
