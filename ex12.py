72cc6935507209d10117f82575fe289e5b6a8d38
PurelyStupidity <micoolplays@gmail.com>
2022-02-15 08:51:58 +1300

    February 15, 2022 8:51 AM

--- ex12.py
+++ ex12.py
@@ -26,7 +26,8 @@ student_price = 9.00
 child_price = 7.00
 gift_price = 0
 
-total_adult
+total_adult_tickets = 0
+
 def get_ticket():
     ticket = input("What type of ticket would you like to buy?\n (A)dult, (S)tudent, (C)hild, or (G)ift Voucher?\nX to cancel").lower()
     if ticket == "a":
