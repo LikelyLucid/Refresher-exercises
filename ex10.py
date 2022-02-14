618ad9e078988fd2caca4a872ec3c474a86da90f
PurelyStupidity <micoolplays@gmail.com>
2022-02-15 08:17:29 +1300

    February 15, 2022 8:17 AM

--- ex10.py
+++ ex10.py
@@ -1,4 +1,4 @@
 """Write a function that takes in a price without GST, calculates the GST of 15% and returns the combined price.
 """
 def calc_gst(price):
-    price * 1.15
+    price = price * 1.15
