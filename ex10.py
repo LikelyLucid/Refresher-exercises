6a5f63cc765fe310c6f69eaaa7633be415cbebd3
PurelyStupidity <micoolplays@gmail.com>
2022-02-15 08:16:52 +1300

    February 15, 2022 8:16 AM

--- ex10.py
+++ ex10.py
@@ -1,3 +1,2 @@
-"""Write a function that takes in a price without GST, calculates the GST of 15% and returns the combined price. See the examples below to understand what is needed and how to get
-currency formatting:
+"""Write a function that takes in a price without GST, calculates the GST of 15% and returns the combined price.
 """
