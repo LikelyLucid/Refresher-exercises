af46a0a23317a8c3720779ae4e98a4dd91dbe1c6
PurelyStupidity <micoolplays@gmail.com>
2022-02-15 08:39:38 +1300

    February 15, 2022 8:39 AM

--- ex12.py
+++ ex12.py
@@ -1,2 +1,25 @@
 """
+Create a program for a movie cinema. It will be used to
+keep track of ticket sales throughout the day. The
+program should do the following:
+•
+•
+Keep asking for tickets until the end of the day
+(maybe ask the user "Do you want to sell
+another ticket? (Y/N) ")
+Tickets sold are either Adult, Student, Child, or
+they use a Gift Voucher.
+o When a ticket is sold, print the price of
+the ticket to the user:
+• Adult $12.50
+Student $9.00
+• Child $7.00
+Gift Voucher is free
+o When a ticket is sold, keep track of how
+many are sold of each type.
+At the end of the day, print out the total tickets
+sold/used, how many tickets were sold of each
+type, and the total value of tickets.
+Advanced challenge: Allow the user to cancel
+buying a ticket
 """
