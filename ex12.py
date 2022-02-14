c40dfdce2698ec87a5a11ed122c5b6969c3b44ea
PurelyStupidity <micoolplays@gmail.com>
2022-02-15 08:57:33 +1300

    February 15, 2022 8:57 AM

--- ex12.py
+++ ex12.py
@@ -21,7 +21,7 @@ type, and the total value of tickets.
 Advanced challenge: Allow the user to cancel
 buying a ticket
 """
-standard_input
+standard_input = "a"
 adult_price = 12.50
 student_price = 9.00
 child_price = 7.00
