bc75d93312b5b294e646c12a54205ce335d7b1f1
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:12:48 +1300

    February 14, 2022 2:12 PM

---
+++ ex5.py
@@ -0,0 +1,4 @@
+"""Write a function numbers_in_list(list, multiple) that takes a list and a number and prints a new list of all
+numbers in the given list that are a multiple of that number. See the examples below to understand what is
+needed :
+"""
