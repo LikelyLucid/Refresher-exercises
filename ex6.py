baea39d9f8294fd29ce45f8cdba9d471348529bc
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:14:28 +1300

    February 14, 2022 2:14 PM

--- ex6.py
+++ ex6.py
@@ -1,5 +1,5 @@
 """Write a function print_name(name, number) that prints a given name a certain number of times:"""
 def print_name(name, number):
-    for I in range(number):
+    for i in range(number):
         print(name)
 
