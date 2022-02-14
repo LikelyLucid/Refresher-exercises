eed162194b572fe2435be3e5202247a1976df9f5
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:18:25 +1300

    February 14, 2022 2:18 PM

--- ex7.py
+++ ex7.py
@@ -1,6 +1,10 @@
 """Write a function print_word(word, number) that prints the given number of letters in uppercase and the rest in lowercase.
 """
 def print_word(word, number):
-    for i in range(word)
+    for i in range(word):
+        if i < number:
+            print(word[i].upper())
+        else:
+            print(word[i].lower())
 
 print(print_word("Hello", 3))
