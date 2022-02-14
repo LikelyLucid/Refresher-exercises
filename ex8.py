26e843aff78fa5e411f05c6f6cae55183f159b89
PurelyStupidity <micoolplays@gmail.com>
2022-02-14 14:32:51 +1300

    February 14, 2022 2:32 PM

--- ex8.py
+++ ex8.py
@@ -5,4 +5,5 @@ def print_word(word, number):
     Prints the given number of letters in uppercase and the rest in lowercase.
     """
     return (word[:number].upper() + word[number:].lower())
+
 print(print_word('Python', 2))
