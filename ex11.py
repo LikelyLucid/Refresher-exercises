4a7aa6b67df11a6457288c0c5ad65132b7b33cc9
PurelyStupidity <micoolplays@gmail.com>
2022-02-15 08:37:02 +1300

    February 15, 2022 8:37 AM

--- ex11.py
+++ ex11.py
@@ -5,4 +5,10 @@ base_fine = 0.5
 per_day_fine = 0.8
 max_fine = 30
 
-def ca
+def calc_fine(days_late):
+    fine = base_fine
+    if days_late > 0:
+        fine = fine + (per_day_fine * days_late)
+        if fine > max_fine:
+            fine = max_fine
+    return fine
