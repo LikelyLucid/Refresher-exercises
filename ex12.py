7209ac77f772394db273927e4a4c5f588bc2c3cb
PurelyStupidity <micoolplays@gmail.com>
2022-02-17 10:22:05 +1300

    February 17, 2022 10:22 AM

--- ex12.py
+++ ex12.py
@@ -1,10 +1,3 @@
-"""
-Create a program for a movie cinema. It will be used to
-keep track of ticket sales throughout the day. The
-program should do the following:
-Keep asking for tickets until the end of the day
-(maybe ask the user "Do you want to sell
-
 adult_price = 12.50
 student_price = 9.00
 child_price = 7.00
