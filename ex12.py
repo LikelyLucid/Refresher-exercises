3f65239b99da80b984e30d12959db0203853599e
PurelyStupidity <micoolplays@gmail.com>
2022-02-17 10:22:00 +1300

    February 17, 2022 10:22 AM

--- ex12.py
+++ ex12.py
@@ -4,23 +4,6 @@ keep track of ticket sales throughout the day. The
 program should do the following:
 Keep asking for tickets until the end of the day
 (maybe ask the user "Do you want to sell
-another ticket? (Y/N) ")
-Tickets sold are either Adult, Student, Child, or
-they use a Gift Voucher.
-When a ticket is sold, print the price of
-the ticket to the user:
-• Adult $12.50
-• Student $9.00
-• Child $7.00
-Gift Voucher is free
-When a ticket is sold, keep track of how
-many are sold of each type.
-At the end of the day, print out the total tickets
-sold/used, how many tickets were sold of each
-type, and the total value of tickets.
-Advanced challenge: Allow the user to cancel
-buying a ticket
-"""
 
 adult_price = 12.50
 student_price = 9.00
