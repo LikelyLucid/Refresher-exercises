f6bad8737b790d062bea151d2164536913dcb06e
PurelyStupidity <micoolplays@gmail.com>
2022-02-18 11:33:31 +1300

    February 18, 2022 11:33 AM

---
+++ Extra2.py
@@ -0,0 +1,7 @@
+"""
+Donald is a gentle soul who loves to read. Unfortunately of the last few years he has developed a horrible
+aversion to the letter "q". Unfortunately he has been asked to look through a huge list of words and this is causing
+him an enormous amount of stress.
+Your task is to write a program that replaces all the letter q's with a letter "p" (which will save Donald a great deal
+of distress)
+"""
