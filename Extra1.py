83b3783efc7c4f460216b2b0db75205f4ce7d6e7
PurelyStupidity <micoolplays@gmail.com>
2022-02-18 11:28:28 +1300

    February 18, 2022 11:28 AM

--- Extra1.py
+++ Extra1.py
@@ -1,4 +1,5 @@
-"""You will be given a series of integer numbers that you must add together. Each line will contain one number
+"""
+You will be given a series of integer numbers that you must add together. Each line will contain one number
 between 1 and 100 and the series will be terminated with a #.
 Your program must output the sum of all the numbers.
 """
