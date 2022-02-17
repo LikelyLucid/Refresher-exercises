572835bceb83cbbb942fdfb61e0daa278e7a6ec8
PurelyStupidity <micoolplays@gmail.com>
2022-02-18 11:35:38 +1300

    February 18, 2022 11:35 AM

--- Extra2.py
+++ Extra2.py
@@ -5,4 +5,4 @@ him an enormous amount of stress.
 Your task is to write a program that replaces all the letter q's with a letter "p" (which will save Donald a great deal
 of distress)
 """
-print((input("enter your inputs").split()))
+input
