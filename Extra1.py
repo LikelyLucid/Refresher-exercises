f8a89e781fd3b982558da997b33168f2751ffbc2
PurelyStupidity <micoolplays@gmail.com>
2022-02-18 11:32:09 +1300

    February 18, 2022 11:32 AM

--- Extra1.py
+++ Extra1.py
@@ -1,8 +1,4 @@
-"""
-You will be given a series of integer numbers that you must add together. Each line will contain one number
-between 1 and 100 and the series will be terminated with a #.
-Your program must output the sum of all the numbers.
-"""
+
 input_list = input("enter your inputs").split()
 sum = 0
 print(input_list)
